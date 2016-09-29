6a1dfe170bfdcfbc639976270304a7534a2a6968
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-29 23:47:56 +0200

    Blog update at 2016-09-29 21:47:56

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 buster generate --domain=http://127.0.0.1:2368
 curl -o static/talks/index.html http://localhost:2368/talks/
+curl -o static/rss/index.html http://localhost:2368/rss/
 
 find static -name *.html -type f -exec sed -i "" -e 's#http://localhost:2368#https://stefanscherer.github.io#g' {} \;
 find static -name *.html -type f -exec sed -i "" -e 's#http://fonts.googleapis.com#https://fonts.googleapis.com#' {} \;
