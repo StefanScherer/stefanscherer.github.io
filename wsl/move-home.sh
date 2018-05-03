003f2da9ed0c39a8f31a8c53b519603d17a4ad3c
Stefan Scherer <scherer_stefan@icloud.com>
2018-05-03 13:38:43 +0200

    Add echo

--- wsl/move-home.sh
+++ wsl/move-home.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-sudo mkdir /Users
+if [ ! -d /Users ]; then
+  echo "Creating /Users"
+  sudo mkdir /Users
+fi
 cd $HOME/..
 windowsuser=$(cmd.exe /c echo %USERNAME% | tr -d '\r')
-sudo cp -r $USER /Users/$windowsuser
-sudo chown $USER:$USER /Users/$windowsuser
-sudo sed -i "s,$HOME,/Users/$windowsuser," /etc/passwd
-exit
+if [ ! -d /Users/$windowsuser ]; then
+  echo "Moving $HOME to /Users/$windowsuser"
+  sudo cp -r $USER /Users/$windowsuser
+  sudo chown $USER:$USER /Users/$windowsuser
+  sudo sed -i "s,$HOME,/Users/$windowsuser," /etc/passwd
+  echo "Now exit this terminal and re-open another shell."
+else
+  echo "Already moved to /Users/$windowsuser"
+fi
