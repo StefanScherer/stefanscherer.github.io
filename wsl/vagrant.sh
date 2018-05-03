8e1e249bd8fa46dbfe43da21200b7652205b68ca
Stefan Scherer <scherer_stefan@icloud.com>
2018-05-03 20:07:18 +0200

    Fix check

--- wsl/vagrant.sh
+++ wsl/vagrant.sh
@@ -6,7 +6,8 @@ if [ ! -e ~/.vagrant.d ]; then
   ln -s /mnt/c/Users/$(cmd.exe /c echo %USERNAME% | tr -d '\r')/.vagrant.d/ ~/.vagrant.d
 fi
 
-if [ "$(dpkg -l | grep vagrant)" == "" ]; then
+v=$(dpkg -l | grep vagrant)
+if [ "$v" == "" ]; then
   echo "Installing Vagrant"
   wget https://releases.hashicorp.com/vagrant/2.0.4/vagrant_2.0.4_x86_64.deb
   sudo dpkg -i vagrant_2.0.4_x86_64.deb
